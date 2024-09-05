b6fb649184bc9c0268f71cc77ce22fa35bf17ac5
Reynard A. <reynardaurele1510@gmail.com>
2024-09-05 10:45:05 +0700

    Arithmetic operators

--- pre-test/day-1/Hello World/src/Main.java
+++ pre-test/day-1/Hello World/src/Main.java
@@ -1,4 +1,9 @@
 public class Main {
+
+    public static double luas(float pi, int r){
+        return pi*r*r;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
@@ -20,11 +25,22 @@ public class Main {
         String myName;
         myName = "Reyy";
 
-        System.out.println("Hello My Name is "+myName);
-        System.out.printf("I'm %d years old \n",age);
-        System.out.println("My salary is "+salary);
-        System.out.println("I sell tissue for: " +price +'$');
-        System.out.println("My grade is: " +nilai);
-        System.out.println("Am I straight: " +status);
+//        System.out.println("Hello My Name is " +myName);
+//        System.out.printf("I'm %d years old \n", age);
+//        System.out.println("My salary is "+salary);
+//        System.out.println("I sell tissue for: " +price +'$');
+//        System.out.println("My grade is: " +nilai);
+//        System.out.println("Am I straight: " +status);
+
+
+        final float pi = 3.14f;
+        int r;
+        r = 10;
+        double l1 = luas(pi,r);
+        System.out.println("Luas adalah "+l1);
+
+        r=15;
+        double l2 = luas(pi,r);
+        System.out.println("Luas adalah "+l2);
     }
 }
