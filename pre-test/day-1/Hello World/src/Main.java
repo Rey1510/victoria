c30c4434385b6b9030b69997b1b9bf3ec97ba7b1
Reynard A. <reynardaurele1510@gmail.com>
2024-09-05 10:31:11 +0700

    data type

--- pre-test/day-1/Hello World/src/Main.java
+++ pre-test/day-1/Hello World/src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int age;
+        age = 20;
+
+        long salary;
+        salary = 0;
+
+        float price;
+        price = 99.9f;
+
+        char nilai;
+        nilai = 'A';
+
+        boolean status;
+        status = true;
+
+        String myName;
+        myName = "Reyy";
+
+        System.out.println("Hello My Name is "+myName);
+        System.out.printf("I'm %d years old \n",age);
+        System.out.println("My salary is "+salary);
+        System.out.println("I sell tissue for " +price +'$');
+        System.out.println("My grade is: " +nilai);
+        System.out.println("Am I straight: " +status);
     }
 }
