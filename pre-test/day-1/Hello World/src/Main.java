0529a1b732727b84159a5e779a2c66a6757bde68
Reynard A. <reynardaurele1510@gmail.com>
2024-09-05 13:11:16 +0700

    Odd only

--- pre-test/day-1/Hello World/src/Main.java
+++ pre-test/day-1/Hello World/src/Main.java
@@ -163,9 +163,10 @@ public class Main {
 //        }
 
         for (int i = 1; i <= 30; i++) {
-            if(i%2==1){
-                System.out.println(i);;
+            if(i%2==0){
+                continue;
             }
+                System.out.println(i);;
         }
 
 
