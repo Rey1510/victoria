df88de7be6f4682aa9acbf8cd1b58b786fa65a19
Reynard A. <reynardaurele1510@gmail.com>
2024-09-05 13:51:20 +0700

    Pindahan dari branch test

---
+++ pre-test/day-1/Hello World/src/Main.java
@@ -0,0 +1,196 @@
+public class Main {
+
+    public static double luas(float pi, int r){
+        return pi*r*r;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+
+        int age;
+        age = 20;
+
+        long salary;
+        salary = 0;
+
+        float price;
+        price = 99.9f;
+
+        char nilai;
+        nilai = 'A';
+
+        boolean status;
+        status = true;
+
+        String myName;
+        myName = "Reyy";
+
+//        System.out.println("Hello My Name is " +myName);
+//        System.out.printf("I'm %d years old \n", age);
+//        System.out.println("My salary is "+salary);
+//        System.out.println("I sell tissue for: " +price +'$');
+//        System.out.println("My grade is: " +nilai);
+//        System.out.println("Am I straight: " +status);
+
+
+//        final float pi = 3.14f;
+//        int r;
+//        r = 10;
+//        double l1 = luas(pi,r);
+//        System.out.println("Luas adalah "+l1);
+//
+//        r=15;
+//        double l2 = luas(pi,r);
+//        System.out.println("Luas adalah "+l2);
+
+//        int angka;
+//        angka = 2;
+//        if(angka%2==0){
+//            System.out.println("Angka genap!");
+//        }else{
+//            System.out.println("Angka ganjil!");
+//        }
+//
+//        angka = 13;
+//        if(angka%2==0){
+//            System.out.println("Angka genap!");
+//        }else{
+//            System.out.println("Angka ganjil!");
+//        }
+
+//        int myNumber = 5;
+//        String myString = "5";
+//        System.out.println(myNumber == myString);
+
+//        int myNumber;
+//        String statusOdd;
+//        myNumber = 10;
+//        if(myNumber%2==0){
+//            statusOdd = "Genap";
+//        }else{
+//            statusOdd = "Ganjil";
+//        }
+//        if(myNumber>50){
+//            System.out.println("Angka lebih besar dari 50  dan angka "+statusOdd);
+//        }else{
+//            System.out.println("Angka lebih kecil dari 50  dan angka "+statusOdd);
+//        }
+//
+//        myNumber = 75;
+//        if(myNumber%2==0){
+//            statusOdd = "Genap";
+//        }else{
+//            statusOdd = "Ganjil";
+//        }
+//        if(myNumber>50){
+//            System.out.println("Angka lebih besar dari 50  dan angka "+statusOdd);
+//        }else{
+//            System.out.println("Angka lebih kecil dari 50  dan angka "+statusOdd);
+//        }
+//
+//        myNumber = 76;
+//        if(myNumber%2==0){
+//            statusOdd = "Genap";
+//        }else{
+//            statusOdd = "Ganjil";
+//        }
+//        if(myNumber>50){
+//            System.out.println("Angka lebih besar dari 50  dan angka "+statusOdd);
+//        }else{
+//            System.out.println("Angka lebih kecil dari 50  dan angka "+statusOdd);
+//        }
+//
+//        myNumber = 47;
+//        if(myNumber%2==0){
+//            statusOdd = "Genap";
+//        }else{
+//            statusOdd = "Ganjil";
+//        }
+//        if(myNumber>50){
+//            System.out.println("Angka lebih besar dari 50  dan angka "+statusOdd);
+//        }else{
+//            System.out.println("Angka lebih kecil dari 50  dan angka "+statusOdd);
+//        }
+
+//        String dayName;
+//        Integer[] day = new Integer[]{1,2,3,5};
+//        for(Integer en : day){
+//            switch (en){
+//                case 1: dayName = "Sunday"; break;
+//                case 2: dayName = "Monday"; break;
+//                case 3: dayName = "Tuesday"; break;
+//                default: dayName = "Invalid day"; break;
+//            }
+//            System.out.println(dayName);
+//        }
+
+//        String oddEven;
+//        Integer[] x = new Integer[]{1,2,3,4,5};
+////        for(Integen : x){
+//            switch(en%2){
+//                case 0: oddEven = "Even"; break;
+//                case 1: oddEven = "Odd"; break;
+//                default: oddEven = ""; break;
+//            }
+//
+//            System.out.println(oddEven);
+//        }
+
+//        for(int i=1; i<=50 ; i++){
+//            if(i%2==0){
+//                System.out.println(i+" is Even");
+//            }else{
+//                System.out.println(i);
+//            }
+//        }
+
+//        int i = 1;
+//        while(i<=50){
+//            if(i%2==0){
+//                System.out.println("Even");
+//            }else{
+//                System.out.println(i);
+//            }
+//            i++;
+//        }
+
+//        for (int i = 1; i < 50; i++) {
+//            if(i%15==0){
+//                break;
+//            }else {
+//                System.out.println(i);
+//            }
+//        }
+
+//        for (int i = 1; i <= 30; i++) {
+//            if(i%2==0){
+//                continue;
+//            }
+//                System.out.println(i);;
+//        }
+
+//        int x = 8;
+//        int answer = 1;
+//        for (int i = 1; i <= x ; i++) {
+//            answer = answer * i;
+//        }
+//        System.out.println(answer);
+
+//        int x = 8;
+//        int answer = 1;
+//        for (int i = 1; x >= i ; x--) {
+//            answer = answer * x;
+//        }
+//        System.out.println(answer);
+
+//        int x = 8;
+//        int answer = 1;
+//        for (int i = x; i >= 1 ; i--) {
+//            answer = answer * i;
+//        }
+//        System.out.println(answer);
+
+
+
+    }
+}
