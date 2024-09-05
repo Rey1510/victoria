a57fe73c670fdd5acfe5c4f5ef92edcf11c94d60
Reynard A. <reynardaurele1510@gmail.com>
2024-09-05 14:09:17 +0700

    -

--- pre-test/day-1/section-2/MyCar/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Car myCar = new Car();
-        myCar.model = "Toyota";
-        myCar.year = 2020;
-        myCar.drive();
-    }
-}
