6ac87944dde61e7b5c4eb658e6dfb09d1986f3fd
Reynard A. <reynardaurele1510@gmail.com>
2024-09-05 15:04:23 +0700

    Calc

---
+++ exercise/day-1/section-2/My-Calculator/src/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+    int a;
+    int b;
+
+    public int getSum(int a, int b){
+        return a + b;
+    }
+
+    public int getSubstract(int a, int b){
+        return a-b;
+    }
+
+    public double getMultiply(int a, int b){
+        return a*b;
+    }
+
+    public double getDivide(int a, int b){
+        if(b==0){
+            System.out.println("Pembagi tidak boleh 0");
+            return 0;
+        }
+        return a/b;
+    }
+
+}
