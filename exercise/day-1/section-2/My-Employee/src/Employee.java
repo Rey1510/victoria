4a74d25f6ab5b29317967df55b6c9aec5ade7b50
Reynard A. <reynardaurele1510@gmail.com>
2024-09-05 14:20:50 +0700

    Employee

---
+++ exercise/day-1/section-2/My-Employee/src/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    String name;
+    String npk;
+    double salary;
+    String address;
+    int age;
+
+    void sayHello(){
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ exercise/day-1/section-2/My-Employee/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee em = new Employee();
+        em.address = "Jalanin Aja Dulu";
+        em.age = 21;
+        em.name = "Rey";
+        em.npk = "68773";
+        em.salary = 100.5;
+
+        em.sayHello();
+    }
+}
