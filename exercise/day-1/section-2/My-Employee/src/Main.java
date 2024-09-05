eb5e2b07a9a4c971e6723eeb8a660f1f7693d65a
Reynard A. <reynardaurele1510@gmail.com>
2024-09-05 14:32:55 +0700

    Manggil dari class lain

--- exercise/day-1/section-2/My-Employee/src/Main.java
+++ exercise/day-1/section-2/My-Employee/src/Main.java
@@ -1,12 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        Employee myEmployee = new Employee();
-        myEmployee.address = "Jalanin Aja Dulu";
-        myEmployee.age = 21;
-        myEmployee.name = "Rey";
-        myEmployee.npk = "68773";
-        myEmployee.salary = 100.5;
+        Employee myEmployee1 = new Employee();
+        myEmployee1.address = "Jalanin Aja Dulu";
+        myEmployee1.age = 21;
+        myEmployee1.name = "Rey";
+        myEmployee1.npk = "68773";
+        myEmployee1.salary = 100.5;
 
-        myEmployee.sayHello();
+        myEmployee1.sayHello();
+
+        System.out.println("Hello " + myEmployee1.name+" " + "Age : " + myEmployee1.age +" " +myEmployee1.npk+" "+ myEmployee1.salary+" " + myEmployee1.address);
+
+        Employee myEmployee2 = new Employee();
+        myEmployee2.address = "Jalan Serta Yesus";
+        myEmployee2.age = 60;
+        myEmployee2.name = "Agus";
+        myEmployee2.npk = "66666";
+        myEmployee2.salary = 90010.52;
+
+        myEmployee2.sayHello();
+
+        System.out.println("Hello " + myEmployee2.name+" " + "Age : " + myEmployee2.age +" " +myEmployee2.npk+" "+ myEmployee2.salary+" " + myEmployee2.address);
+
     }
 }
