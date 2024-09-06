07df263cc1c409110abe163c16ea50ddd84dc56a
Reynard A. <reynardaurele1510@gmail.com>
2024-09-06 11:07:49 +0700

    -

--- exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
+++ exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.first_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class FirstApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FirstApplication.class, args);
 	}
 
 }
