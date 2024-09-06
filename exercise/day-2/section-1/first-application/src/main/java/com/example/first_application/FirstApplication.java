8327171810b35ff698d5257a4125479cdab40f10
Reynard A. <reynardaurele1510@gmail.com>
2024-09-06 13:45:40 +0700

    -

--- exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
+++ exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -19,16 +20,22 @@ public class FirstApplication {
 		SpringApplication.run(FirstApplication.class, args);
 	}
 
-	@GetMapping(){
-		return "Hello World!"
+	@GetMapping()
+	public String sayHello(){
+		return "Hello World!";
+	}
+
+	@GetMapping("/users")
+	public List<String> getUser(){
+		return List.of("Jon","Jane","Jack");
 	}
 
 	@GetMapping("/users/list")
-	public ArrayList<GetAssetResponse> sayHello(){
+	public ArrayList<GetAssetResponse> listAll(){
 
 		ArrayList<GetAssetResponse> list = new ArrayList<>();
-		var response = GetAssetResponse.builder().id(10l).fullName("Jon").age(20).address("Jalanin Aja").build();
-		var response1 = GetAssetResponse.builder().id(11l).fullName(("Anton")).age(19).address("Jalan Serta Yesus").build();
+		var response = GetAssetResponse.builder().id(1l).fullName("Jon").age(20).address("Jalanin Aja").build();
+		var response1 = GetAssetResponse.builder().id(2l).fullName(("Anton")).age(19).address("Jalan Serta Yesus").build();
 
 		list.add(response);
 		list.add(response1);
@@ -37,14 +44,36 @@ public class FirstApplication {
 	}
 //		response.setFullName("Testestes");
 
-	@GetMapping("/users")
-	public List<String> getUser(){
-		return List.of("Jon","Jane","Jack");
+	@GetMapping("/users/{id}")
+	public GetAssetResponse getAssetbyId(
+			@PathVariable Long id
+	){
+		ArrayList<GetAssetResponse> asset = new ArrayList<>();
+
+		asset.add(GetAssetResponse.builder().id(1l).fullName("Anton").build());
+		asset.add(GetAssetResponse.builder().id(2l).fullName("Anton2").build());
+		asset.add(GetAssetResponse.builder().id(3l).fullName("Anton3").build());
+		asset.add(GetAssetResponse.builder().id(4l).fullName("Anton4").build());
+		asset.add(GetAssetResponse.builder().id(5l).fullName("Anton5").build());
+
+//		for (int i = 0; i < asset.size(); i++) {
+//			if(asset.get(i).getId().equals(id)){
+//				return asset.get(i);
+//			}
+//		}
+
+		for(GetAssetResponse as : asset){
+			if(as.getId().equals(id)){
+				return as;
+			}
+		}
+		return null;
+//		return GetAssetResponse.builder().id(id).fullName("Jontor").build();
 	}
 
-	@GetMapping("/users/{id}")
-	public ResponseEntity<Long> getUserById(Long id){
-		return new ResponseEntity<>(id, HttpStatus.OK);
-	}
+//	@GetMapping("/users/{id}")
+//	public ResponseEntity<Long> getUserById(Long id){
+//		return new ResponseEntity<>(id, HttpStatus.OK);
+//	}
 
 }
