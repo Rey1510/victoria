d433f3410628fc92cad7420e0d15cec49f522839
Reynard A. <reynardaurele1510@gmail.com>
2024-09-06 14:20:39 +0700

    param

--- exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
+++ exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -73,6 +75,14 @@ public class FirstApplication {
 		return all;
 
 //		return GetAssetResponse.builder().id(id).fullName("Jontor").build();
+	}
+
+	@GetMapping("/calc/add")
+	public String getAdd(
+			@RequestParam(name= "panjang") int panjang,
+			@RequestParam(name= "lebar",required = false,defaultValue = "20D") int lebar
+	){
+		return String.valueOf(panjang + lebar);
 	}
 
 //	@GetMapping("/users/{id}")
