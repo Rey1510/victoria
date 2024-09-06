04d3a5bfdbf29c3d394bddb48146fc2b4664a427
Reynard A. <reynardaurele1510@gmail.com>
2024-09-06 11:21:02 +0700

    -

--- exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
+++ exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
@@ -2,12 +2,24 @@ package com.example.first_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class FirstApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApplication.class, args);
+	}
+
+	@GetMapping
+	public String sayHello(){
+		return "Hello World";
+	}
+
+	@GetMapping("/users"){
+		public
 	}
 
 }
